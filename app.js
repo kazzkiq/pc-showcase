7b908ae85476cfef07341d9ebd31e2867a8fb3fa
Kazzkiq <claudiohw@gmail.com>
2017-03-12 18:25:19 -0300

    Updating title

--- app.js
+++ app.js
@@ -3453,7 +3453,7 @@ riot.tag2('comp-footer', '<div class="container"> <div class="FooterComp"> <div 
 });
 
 require.register("components/MainComp.tag", function(exports, require, module) {
-riot.tag2('comp-main', '<comp-topbar></comp-topbar> <div class="hero"> <img src="imgs/hero-icons.png" class="hero-img" alt="Create anything with PeerAssets!"> <h1>Welcome to our showcase of projects, powered by <span class="primary">Peercoin</span> and <span class="primary">PeerAssets</span>.</h1> <h2>You can browse betwen projects, or <a href="#" class="primary">submit</a> your own.</h2> </div> <comp-cards></comp-cards>', '', '', function(opts) {
+riot.tag2('comp-main', '<comp-topbar></comp-topbar> <div class="hero"> <img src="imgs/hero-icons.png" class="hero-img" alt="Create anything with PeerAssets!"> <h1>Project showcase.</h1> <h2>You can browse betwen projects, or <a href="#" class="primary">submit</a> your own.</h2> </div> <comp-cards></comp-cards>', '', '', function(opts) {
 });
 });
 
